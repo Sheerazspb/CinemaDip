d9cacf94e5404e2608dc858d0ac530c6c68be382
sheerazspb <sheerazspb@gmail.com>
2020-11-17 04:42:05 +0300

    remove base64_encode

--- client/qrOrderUpdate.php
+++ client/qrOrderUpdate.php
@@ -1,7 +1,7 @@
 <?php
 require '../admin/connection.php';
 $id = $_POST['nId'];
-$qr = base64_encode(serialize($_POST['nQr']));
+$qr = $_POST['nQr'];
 $sql = "UPDATE  makeOrder SET qrCode = ('$qr') WHERE id = $id";
 $res = mysqli_query($conn, $sql);
 if ($res) {
